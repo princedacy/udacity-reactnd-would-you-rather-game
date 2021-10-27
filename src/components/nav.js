6c6de344c6ecb9b74b387f8896d25ea006f30807
princedacy <princedacy@gmail.com>
2021-10-27 15:22:18 +0200

    feat: implement logout functionality

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,33 +1,60 @@
-import React, { Component } from "react";
-
+import React, { Component, Fragment } from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutAuthedUser } from '../actions/authedUser'
+import { Button, Avatar, Image } from "antd";
 class Nav extends Component {
-    
+    logout() {
+        localStorage.removeItem('user');
+        this.props.dispatch(logoutAuthedUser)
+        this.props.history.push('/login')
+    }
+    login() {
+        console.log(this.props.history.push('/login'))
+    }
     render() {
+        const { user, isAuth } = this.props
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <NavLink className="nav-link" to='/'>Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">New Question</a>
+                            <NavLink className="nav-link" to='/add'>New Question</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Leader Board</a>
+                            <NavLink className="nav-link" to='/leaderboard'>Leader Board</NavLink>
                         </li>
                     </ul>
-                    <span className="navbar-text">
-                        Hello, Tyler McGinnis
-                    </span>
-                    <form className="form-inline my-2 my-lg-0">
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
-                    </form>
+                    <div className='d-flex'>
+                        {isAuth ? (
+                            <Fragment>
+                                <div className='autheduser-initials mr-5'>
+                                    <p className="navbar-text pr-2">
+                                        Hello, {user && user.name}
+                                    </p>
+                                    <Avatar src={<Image src={user && user.avatarURL} style={{ width: 32 }} />} style={{ width: 32, height: 32 }} />
+                                </div>
+                                <Button type="primary" onClick={this.logout}>Log out</Button>
+
+                            </Fragment>
+                        ) : (
+                            <Button type="primary" onClick={this.login}>Log in</Button>
+                        )}
+                    </div>
+
                 </div>
             </nav>
         )
     }
 }
-
-export default Nav
+function mapStateToProps({ authedUser, users }) {
+    return {
+        isAuth: authedUser !== null,
+        user: users[authedUser]
+    }
+}
+export default withRouter(connect(mapStateToProps)(Nav))
